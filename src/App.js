7bdba4ac5630e2d6012df3c47946aedfbfaed30a
Elchurianil <aofficial1092@gmail.com>
2024-12-06 20:41:02 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import './RegisterForm.js';
+
+function App() {
+  return (
+    <>
+    
+    
+     <h4>anil elchuri</h4>
+    <RegisterForm/>
+
+    
+    </>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/component/RegisterForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RegisterForm.css';
+
+function RegisterForm(){
+    return(
+        <React.Fragment>
+
+            <h2>registration  form </h2>
+        </React.Fragment>
+    );
+}
+export default RegisterForm;
